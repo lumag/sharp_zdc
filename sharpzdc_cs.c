9c63a5bbf5037c702ba35442718cd14013ccb31c
Dmitry Baryshkov <dbaryshkov@gmail.com>
2008-10-14 17:17:52 +0400

    v4l part update: I got emulated image
    Signed-off-by: Dmitry Baryshkov <dbaryshkov@gmail.com>

--- sharpzdc_cs.c
+++ sharpzdc_cs.c
@@ -4,9 +4,13 @@
 #include <linux/fs.h>
 #include <linux/kref.h>
 #include <linux/version.h>
+#include <linux/spinlock.h>
+#include <linux/mutex.h>
+#include <linux/interrupt.h>
 
 #include <media/v4l2-dev.h>
 #include <media/v4l2-ioctl.h>
+#include <media/videobuf-vmalloc.h>
 
 #include <pcmcia/cs_types.h>
 #include <pcmcia/cs.h>
@@ -14,13 +18,165 @@
 #include <pcmcia/cisreg.h>
 #include <pcmcia/ds.h>
 
+static unsigned int vid_limit = 16;	/* Video memory limit, in Mb */
+module_param(vid_limit, int, 0644);
+MODULE_PARM_DESC(vid_limit, "capture memory limit in megabytes");
+
 struct sharpzdc_info {
 	struct kref		ref;
 	struct pcmcia_device	*p_dev;
 	struct video_device	*vdev;
+
+
+
+	struct videobuf_queue	vb_vidq;
+	struct list_head	queued;
+	spinlock_t		lock;
+
+	unsigned int		width, height;
+	unsigned int		bpl, size;
 };
 
 #define to_zdcinfo(r)	container_of(r, struct sharpzdc_info, ref)
+
+static void sharpzdc_fillbuff(struct sharpzdc_info* info, struct videobuf_buffer *vb)
+{
+	int h , pos = 0;
+	int hmax  = vb->height;
+	int wmax  = vb->width;
+	void *vbuf = videobuf_to_vmalloc(vb);
+
+	pr_debug("%s\n", __func__);
+
+	if (!vbuf)
+		return;
+
+	for (h = 0; h < hmax; h++) {
+		memset(vbuf + pos, 0xcc, wmax * 2);
+		pos += wmax*2;
+	}
+
+	/* Advice that buffer was filled */
+	vb->field_count += 2; /* two fields */
+	do_gettimeofday(&vb->ts);
+	vb->state = VIDEOBUF_DONE;
+}
+
+static void sharpzdc_thread_tick(struct sharpzdc_info *info)
+{
+	struct videobuf_buffer *vb;
+
+	unsigned long flags = 0;
+
+	pr_debug("%s\n", __func__);
+
+	spin_lock_irqsave(&info->lock, flags);
+	if (list_empty(&info->queued))
+		goto unlock;
+
+	vb = list_entry(info->queued.next,
+			 struct videobuf_buffer, queue);
+
+	/* Nobody is waiting on this buffer, return */
+	if (!waitqueue_active(&vb->done))
+		goto unlock;
+
+	list_del(&vb->queue);
+
+	do_gettimeofday(&vb->ts);
+
+	/* Fill buffer */
+	sharpzdc_fillbuff(info, vb);
+
+	wake_up(&vb->done);
+unlock:
+	spin_unlock_irqrestore(&info->lock, flags);
+	return;
+}
+
+
+static void sharpzdc_buf_release(struct videobuf_queue *q,
+		struct videobuf_buffer *vb)
+{
+	pr_debug("%s\n", __func__);
+	BUG_ON(in_interrupt());
+
+	videobuf_vmalloc_free(vb);
+	vb->state = VIDEOBUF_NEEDS_INIT;
+}
+
+static int sharpzdc_buf_setup(struct videobuf_queue *q,
+		unsigned int *count, unsigned int *size)
+{
+	struct sharpzdc_info  *info = q->priv_data;
+	pr_debug("%s\n", __func__);
+
+	*size = info->width * info->height*2;
+
+	if (*count == 0)
+		*count = 32;
+
+	while (*size * *count > vid_limit * 1024 * 1024)
+		(*count)--;
+
+	return 0;
+}
+
+static int sharpzdc_buf_prepare(struct videobuf_queue *q,
+		struct videobuf_buffer *vb,
+		enum v4l2_field field)
+{
+	struct sharpzdc_info     *info  = q->priv_data;
+	int rc;
+	pr_debug("%s\n", __func__);
+
+	/* FIXME: width/height IRT rotation and zoom */
+	if (info->width  < 32 || info->width  > 640 ||
+	    info->height < 32 || info->height > 480)
+		return -EINVAL;
+
+	vb->size = info->width*info->height*2;
+	if (0 != vb->baddr  &&  vb->bsize < vb->size)
+		return -EINVAL;
+
+	/* These properties only change when queue is idle, see s_fmt */
+	vb->width  = info->width;
+	vb->height = info->height;
+	vb->field  = field;
+
+	if (vb->state == VIDEOBUF_NEEDS_INIT) {
+		rc = videobuf_iolock(q, vb, NULL);
+		if (rc < 0)
+			goto fail;
+	}
+
+	vb->state = VIDEOBUF_PREPARED;
+
+	return 0;
+
+fail:
+	sharpzdc_buf_release(q, vb);
+	return rc;
+}
+
+static void sharpzdc_buf_queue(struct videobuf_queue *q,
+		struct videobuf_buffer *vb)
+{
+	struct sharpzdc_info *info = q->priv_data;
+
+	pr_debug("%s\n", __func__);
+
+	vb->state = VIDEOBUF_QUEUED;
+	list_add_tail(&vb->queue, &info->queued);
+}
+
+static struct videobuf_queue_ops sharpzdc_video_qops = {
+	.buf_setup      = sharpzdc_buf_setup,
+	.buf_prepare    = sharpzdc_buf_prepare,
+	.buf_queue      = sharpzdc_buf_queue,
+	.buf_release    = sharpzdc_buf_release,
+};
+
 
 static void sharpzdc_info_release(struct kref *ref)
 {
@@ -42,6 +198,40 @@ static void sharpzdc_vdev_release(struct video_device *vdev)
 	kref_put(&info->ref, sharpzdc_info_release);
 }
 
+static ssize_t
+sharpzdc_read(struct file *file, char __user *data, size_t count, loff_t *ppos)
+{
+	struct sharpzdc_info *info = file->private_data;
+	pr_debug("%s\n", __func__);
+
+	return videobuf_read_stream(&info->vb_vidq, data, count, ppos, 0,
+					file->f_flags & O_NONBLOCK);
+}
+
+static unsigned int
+sharpzdc_poll(struct file *file, struct poll_table_struct *wait)
+{
+	struct sharpzdc_info *info = file->private_data;
+	int ret;
+
+	pr_debug("%s\n", __func__);
+
+	ret = videobuf_poll_stream(file, &info->vb_vidq, wait);
+
+	sharpzdc_thread_tick(info);
+
+	return ret;
+}
+
+static int sharpzdc_mmap(struct file *file, struct vm_area_struct *vma)
+{
+	struct sharpzdc_info  *info = file->private_data;
+	pr_debug("%s\n", __func__);
+
+	return videobuf_mmap_mapper(&info->vb_vidq, vma);
+}
+
+
 static int sharpzdc_open(struct inode *inode, struct file *fp)
 {
 	struct video_device *vdev = video_devdata(fp);
@@ -52,6 +242,11 @@ static int sharpzdc_open(struct inode *inode, struct file *fp)
 	kref_get(&info->ref);
 	fp->private_data = info;
 
+	videobuf_queue_vmalloc_init(&info->vb_vidq, &sharpzdc_video_qops,
+			NULL, &info->lock, V4L2_BUF_TYPE_VIDEO_CAPTURE, V4L2_FIELD_NONE,
+			sizeof(struct videobuf_buffer), info);
+
+
 	return 0;
 }
 
@@ -61,6 +256,9 @@ static int sharpzdc_release(struct inode *inode, struct file *fp)
 
 	pr_debug("%s\n", __func__);
 
+	videobuf_stop(&info->vb_vidq);
+	videobuf_mmap_free(&info->vb_vidq);
+
 	kref_put(&info->ref, sharpzdc_info_release);
 	return 0;
 }
@@ -69,6 +267,9 @@ static struct file_operations sharpzdc_fops = {
 	.owner		= THIS_MODULE,
 	.open		= sharpzdc_open,
 	.release	= sharpzdc_release,
+	.read		= sharpzdc_read,
+	.poll		= sharpzdc_poll,
+	.mmap		= sharpzdc_mmap,
 	.ioctl		= video_ioctl2,
 #ifdef CONFIG_COMPAT
 	.compat_ioctl	= v4l_compat_ioctl32,
@@ -76,23 +277,25 @@ static struct file_operations sharpzdc_fops = {
 	.llseek		= no_llseek,
 };
 
-static int sharpzdc_querycap(struct file *file, void *priv,
+static int sharpzdc_querycap(struct file *file, void *private_data,
 		struct v4l2_capability *cap)
 {
-	struct sharpzdc_info *info = priv;
+	struct sharpzdc_info *info = private_data;
 
+	pr_debug("%s\n", __func__);
 	strcpy(cap->driver, "sharpzdc_cs");
 	strcpy(cap->card, "CE-AG06");
 	snprintf(cap->bus_info, sizeof(cap->bus_info),
 			"pcmcia:%s", dev_name(&info->p_dev->dev));
 	cap->version = KERNEL_VERSION(0, 0, 1);
-	cap->capabilities = V4L2_CAP_VIDEO_CAPTURE;
+	cap->capabilities = V4L2_CAP_VIDEO_CAPTURE | V4L2_CAP_READWRITE;
 	return 0;
 }
 
 static int sharpzdc_enum_input(struct file *file, void *private_data,
 		struct v4l2_input *input)
 {
+	pr_debug("%s\n", __func__);
 	if (input->index > 0)
 		return -EINVAL;
 
@@ -106,6 +309,7 @@ static int sharpzdc_enum_input(struct file *file, void *private_data,
 static int sharpzdc_g_input(struct file *file, void *private_data,
 		unsigned int *index)
 {
+	pr_debug("%s\n", __func__);
 	*index = 0;
 	return 0;
 }
@@ -113,6 +317,7 @@ static int sharpzdc_g_input(struct file *file, void *private_data,
 static int sharpzdc_s_input(struct file *file, void *private_data,
 		unsigned int index)
 {
+	pr_debug("%s\n", __func__);
 	if (index != 0)
 		return -EINVAL;
 
@@ -122,6 +327,7 @@ static int sharpzdc_s_input(struct file *file, void *private_data,
 static int sharpzdc_enum_fmt_vid_cap(struct file *file, void *private_data,
 		struct v4l2_fmtdesc *f)
 {
+	pr_debug("%s\n", __func__);
 	if (f->index > 0)
 		return -EINVAL;
 
@@ -134,13 +340,15 @@ static int sharpzdc_enum_fmt_vid_cap(struct file *file, void *private_data,
 static int sharpzdc_g_fmt_vid_cap(struct file *file, void *private_data,
 		struct v4l2_format *f)
 {
-	// FIXME: width, height, bytesperline, sizeimage,
-#if 1
-	f->fmt.pix.width = 320;
-	f->fmt.pix.height = 240;
-	f->fmt.pix.bytesperline = f->fmt.pix.width * 2;
-	f->fmt.pix.sizeimage = f->fmt.pix.bytesperline * f->fmt.pix.height;
-#endif
+	struct sharpzdc_info *info = private_data;
+
+	pr_debug("%s\n", __func__);
+
+	f->fmt.pix.width = info->width;
+	f->fmt.pix.height = info->height;
+	f->fmt.pix.bytesperline = info->bpl;
+	f->fmt.pix.sizeimage = info->size;
+
 	f->fmt.pix.pixelformat = V4L2_PIX_FMT_RGB565;
 	f->fmt.pix.field = V4L2_FIELD_NONE;
 	f->fmt.pix.colorspace = V4L2_COLORSPACE_SRGB;
@@ -151,6 +359,7 @@ static int sharpzdc_g_fmt_vid_cap(struct file *file, void *private_data,
 static int sharpzdc_try_fmt_vid_cap(struct file *file, void *private_data,
 		struct v4l2_format *f)
 {
+	pr_debug("%s\n", __func__);
 	// FIXME: width, height, bytesperline, sizeimage limitation wrt rotating and zoom.
 	if (f->fmt.pix.width <= 0 || f->fmt.pix.width >= 640 ||
 	    f->fmt.pix.height <= 0 || f->fmt.pix.height >= 480)
@@ -172,15 +381,89 @@ static int sharpzdc_try_fmt_vid_cap(struct file *file, void *private_data,
 static int sharpzdc_s_fmt_vid_cap(struct file *file, void *private_data,
 		struct v4l2_format *f)
 {
-	// FIXME: check for capturing status and return -EBUSY
+	struct sharpzdc_info *info = private_data;
+	int ret;
 
-	int ret = sharpzdc_try_fmt_vid_cap(file, private_data, f);
+	pr_debug("%s\n", __func__);
+	ret = sharpzdc_try_fmt_vid_cap(file, private_data, f);
 	if (ret < 0)
 		return ret;
 
-	// FIXME: apply
+	mutex_lock(&info->vb_vidq.vb_lock);
+	mutex_unlock(&info->vb_vidq.vb_lock);
+
+	// FIXME: width, height, bytesperline, sizeimage limitation wrt rotating and zoom.
+	info->width = f->fmt.pix.width;
+	info->height = f->fmt.pix.height;
 
 	return 0;
+}
+
+static int sharpzdc_reqbufs(struct file *file, void *private_data,
+		struct v4l2_requestbuffers *p)
+{
+	struct sharpzdc_info  *info = private_data;
+
+	pr_debug("%s\n", __func__);
+	return (videobuf_reqbufs(&info->vb_vidq, p));
+}
+
+static int sharpzdc_querybuf(struct file *file, void *private_data,
+		struct v4l2_buffer *p)
+{
+	struct sharpzdc_info  *info = private_data;
+
+	pr_debug("%s\n", __func__);
+	return (videobuf_querybuf(&info->vb_vidq, p));
+}
+
+static int sharpzdc_qbuf(struct file *file, void *private_data, struct v4l2_buffer *p)
+{
+	struct sharpzdc_info *info = private_data;
+
+	pr_debug("%s\n", __func__);
+	return (videobuf_qbuf(&info->vb_vidq, p));
+}
+
+static int sharpzdc_dqbuf(struct file *file, void *private_data, struct v4l2_buffer *p)
+{
+	struct sharpzdc_info  *info = private_data;
+
+	pr_debug("%s\n", __func__);
+	return (videobuf_dqbuf(&info->vb_vidq, p,
+				file->f_flags & O_NONBLOCK));
+}
+
+#ifdef CONFIG_VIDEO_V4L1_COMPAT
+static int sharpzdc_cgmbuf(struct file *file, void *private_data, struct video_mbuf *mbuf)
+{
+	struct sharpzdc_info  *info = private_data;
+
+	pr_debug("%s\n", __func__);
+	return videobuf_cgmbuf(&info->vb_vidq, mbuf, 8);
+}
+#endif
+
+static int sharpzdc_streamon(struct file *file, void *private_data, enum v4l2_buf_type i)
+{
+	struct sharpzdc_info  *info = private_data;
+
+	if (i != V4L2_BUF_TYPE_VIDEO_CAPTURE)
+		return -EINVAL;
+
+	pr_debug("%s\n", __func__);
+	return videobuf_streamon(&info->vb_vidq);
+}
+
+static int sharpzdc_streamoff(struct file *file, void *private_data, enum v4l2_buf_type i)
+{
+	struct sharpzdc_info  *info = private_data;
+
+	pr_debug("%s\n", __func__);
+	if (i != V4L2_BUF_TYPE_VIDEO_CAPTURE)
+		return -EINVAL;
+
+	return videobuf_streamoff(&info->vb_vidq);
 }
 
 
@@ -193,6 +476,16 @@ static struct v4l2_ioctl_ops sharpzdc_ioctl_ops = {
 	.vidioc_g_fmt_vid_cap	= sharpzdc_g_fmt_vid_cap,
 	.vidioc_s_fmt_vid_cap	= sharpzdc_s_fmt_vid_cap,
 	.vidioc_try_fmt_vid_cap	= sharpzdc_try_fmt_vid_cap,
+
+	.vidioc_reqbufs		= sharpzdc_reqbufs,
+	.vidioc_querybuf	= sharpzdc_querybuf,
+	.vidioc_qbuf		= sharpzdc_qbuf,
+	.vidioc_dqbuf		= sharpzdc_dqbuf,
+	.vidioc_streamon	= sharpzdc_streamon,
+	.vidioc_streamoff	= sharpzdc_streamoff,
+#ifdef CONFIG_VIDEO_V4L1_COMPAT
+	.vidiocgmbuf		= sharpzdc_cgmbuf,
+#endif
 };
 
 #define CS_CHECK(fn, ret) \
@@ -341,6 +634,8 @@ static int sharpzdc_probe(struct pcmcia_device *link)
 		return -ENOMEM;
 
 	kref_init(&info->ref);
+	spin_lock_init(&info->lock);
+	INIT_LIST_HEAD(&info->queued);
 
 	info->p_dev = link;
 	link->priv = info;
@@ -361,6 +656,9 @@ static int sharpzdc_probe(struct pcmcia_device *link)
 	}
 	kref_get(&info->ref);
 	video_set_drvdata(info->vdev, info);
+
+	info->width = 320;
+	info->height = 240;
 
 	info->vdev->parent = &link->dev;
 	info->vdev->fops = &sharpzdc_fops;
